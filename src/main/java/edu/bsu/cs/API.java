125690d57370f48005fbc0ab714097125465e2a7
sammibroomi <samuelbrumley@icloud.com>
2024-03-13 22:40:40 -0400

    complete API class that includes functions to connect to the HTTP server and generate an access token to the Spotify API - will move to dedicated classes later.

--- src/main/java/edu/bsu/cs/API.java
+++ src/main/java/edu/bsu/cs/API.java
@@ -1,20 +1,50 @@
 package edu.bsu.cs;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class API {
 
-    public String getToken(){
-        String auth_string = "8f422087aab84a9eb15f-be7e2547c4066" + ":" + "bf46c158e-f9ce935f7c6972d52af";
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String connection = connection();
+    }
+
+    public static String getToken() {
+        String auth_string = "8f422087aab84a9eb15fb7e2547c4066" + ":" + "bf46c158ef9f49ce935f7c6972d5d2a5";
         String auth_bytes = encode(auth_string);
         return auth_bytes;
     }
-    public String encode(String string){
+
+    public static String encode(String string) {
         byte[] bytes = string.getBytes(StandardCharsets.UTF_8);
         String base64EncodedString = Base64.getEncoder().encodeToString(bytes);
-        System.out.println(base64EncodedString);
         return base64EncodedString;
     }
+    public static String connection() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://accounts.spotify.com/api/token"))
+                .header("Authorization", "Basic " + getToken())
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .POST(HttpRequest.BodyPublishers.ofString("grant_type=client_credentials"))
+                .build();
 
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        //System.out.println(response);
+        if (response.statusCode() == 200) {
+            String body = response.body();
+            int startIndex = body.indexOf("access_token") + 13;
+            int endIndex = body.indexOf(",", startIndex);
+            //System.out.println(body.substring(startIndex, endIndex));
+            System.out.println(response.body());
+
+
+        }
+        return null;
+    }
 }
