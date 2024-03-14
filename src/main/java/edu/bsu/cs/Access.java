064a04b4657f32ecfe55301c658551c4e8e40e74
sammibroomi <samuelbrumley@icloud.com>
2024-03-14 10:43:37 -0400

    edit/refactor Access class

--- src/main/java/edu/bsu/cs/Access.java
+++ src/main/java/edu/bsu/cs/Access.java
@@ -9,22 +9,18 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class Access {
-    //main class is only here for testing purposes. connection() should be called by another function later in the project
-    public static void main(String[] args) throws IOException, InterruptedException {
-        String connection = connection();
-    }
 
-    public static String createClientInfo() {
+    public String createClientInfo() {
         String auth_string = "8f422087aab84a9eb15fb7e2547c4066" + ":" + "bf46c158ef9f49ce935f7c6972d5d2a5";
         return encode(auth_string);
     }
 
-    public static String encode(String string) {
+    public String encode(String string) {
         byte[] bytes = string.getBytes(StandardCharsets.UTF_8);
         return Base64.getEncoder().encodeToString(bytes);
     }
 
-    public static String connection() throws IOException, InterruptedException {
+    public String connection() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://accounts.spotify.com/api/token"))
@@ -34,16 +30,14 @@ public class Access {
                 .build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        //System.out.println(response);
+
         if (response.statusCode() == 200) {
-            String body = response.body();
-            int startIndex = body.indexOf("access_token") + 13;
-            int endIndex = body.indexOf(",", startIndex);
-            //System.out.println(body.substring(startIndex, endIndex));
+            String responseBody = response.body();
             System.out.println(response.body());
-
-
+            return response.body();
+        } else {
+            //errorCatch();
         }
-        return null;
+        return response.body();
     }
 }
