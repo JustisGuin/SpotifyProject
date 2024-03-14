2c375e0d72b38580c531db6ebea20d10a95094cc
JustisGuin <jguin1850@gmail.com>
2024-03-13 21:08:48 -0500

    AssertEquals added for APITest

--- src/test/java/edu/bs/cs/APITest.java
+++ src/test/java/edu/bs/cs/APITest.java
@@ -1,11 +1,22 @@
 package edu.bs.cs;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class APITest {
 
-    String clientID = "8f422087aab84a9eb15f-be7e2547c4066";
-    String clientSecert = "bf46c158e-f9ce935f7c6972d52af";
+    @Test
+
+    public void getClient() throws Exception{
+
+        String clientID = "8f422087aab84a9eb15f-be7e2547c4066";
+        String clientSecret= "bf46c158e-f9ce935f7c6972d52af";
 
 
 
 
+        Assertions.assertEquals("8f422087aab84a9eb15f-be7e2547c4066",clientID);
+        Assertions.assertEquals("bf46c158e-f9ce935f7c6972d52af",clientSecret);
+
+    }
 }
