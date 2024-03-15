f17949e46d6d788bd90960f0b8fb3cf0a8b08192
JustisGuin <71509622+JustisGuin@users.noreply.github.com>
2024-03-15 11:39:41 -0400

    Contoller code added: Spotify User Input Code Written, no connection to the API with asking for certain Albums,Songs ect.

--- src/main/java/edu/bsu/cs/Controller.java
+++ src/main/java/edu/bsu/cs/Controller.java
@@ -2,4 +2,25 @@ package edu.bsu.cs;
 import java.util.Scanner;
 
 public class Controller {
+    public static void spotifyInput(){
+        String userInput;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.println("Enter a Artist Name, Songs, and or an Album you which to search.\n");
+            userInput = scanner.nextLine();
+
+            if (userInput.isEmpty()) {
+                System.err.println("No entry was entered.\n");
+            } else if (userInput.equalsIgnoreCase("Quit")) {
+                break;
+            } else {
+                // Perform the desired search operation here
+                System.out.println("Searching for: " + userInput);
+            }
+        } while (true);
+        scanner.close();
+
+    }
 }
+
