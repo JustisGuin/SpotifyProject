16d3a5b78fd6ffa4a22f56d9fc58b8d9758cda90
eramsell2 <ethanramsell502@gmail.com>
2024-03-15 22:57:31 -0400

    create formatter class and functions

--- src/main/java/edu/bsu/cs/Formatter.java
+++ src/main/java/edu/bsu/cs/Formatter.java
@@ -1,4 +1,23 @@
 package edu.bsu.cs;
 
+import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
+
 public class Formatter {
+    public static String formatFollowers(String jsonData){
+        JSONArray followers = (JSONArray) JsonPath.read(jsonData, "followers");
+        System.out.println("Artist Followers: ");
+        return jsonData;
+    }
+    public static String formatGenres(String jsonData){
+        JSONArray genres = (JSONArray) JsonPath.read(jsonData,"genres");
+        System.out.println("Artist's Genres: ");
+        return jsonData;
+    }
+    public static String formatPopularity(String jsonData){
+        JSONArray popularity = (JSONArray) JsonPath.read(jsonData, "popularity");
+        System.out.println("Artists Popularity Rank: ");
+        return jsonData;
+    }
+
 }
