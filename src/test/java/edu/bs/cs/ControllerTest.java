d3662708147bcb23a44afec3ae2eeb7c4ed44b97
JustisGuin <71509622+JustisGuin@users.noreply.github.com>
2024-03-15 20:15:28 -0400

    Main cleared, ControllerTest class started

--- src/test/java/edu/bs/cs/ControllerTest.java
+++ src/test/java/edu/bs/cs/ControllerTest.java
@@ -1,7 +1,26 @@
 package edu.bs.cs;
 
 
+import edu.bsu.cs.Controller;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.naming.ldap.Control;
+
+import java.util.Scanner;
+
+import static edu.bsu.cs.Controller.spotifyInput;
 
 public class ControllerTest {
+    @Test
+    public void getControllerInput(){
+
+        Controller controllerInput = new Controller();
+
+        String spotifyAPIOutPut = controllerInput.spotifyInput(new Scanner(System.in));
+
+        Assertions.assertEquals(result);
+    }
+
 
 }
