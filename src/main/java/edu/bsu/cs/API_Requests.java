64125897675bd32c1198abb9d4d97e71dd2f05ae
sammibroomi <samuelbrumley@icloud.com>
2024-03-17 23:30:16 -0400

    add new functions to APIRequest class (searchForArtist, searchForSong, getArtistFromID, getTrackFromID)

--- src/main/java/edu/bsu/cs/API_Requests.java
+++ src/main/java/edu/bsu/cs/API_Requests.java
@@ -1,6 +1,5 @@
 package edu.bsu.cs;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.IOException;
 import java.net.URI;
@@ -26,6 +25,68 @@ public class API_Requests {
             String responseBody = response.body();
             JSONObject jsonObject = new JSONObject(responseBody);
 
+            return responseBody;
+        }
+        return null;
+    }
+
+    public String searchForSong(String token, String songName) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.spotify.com/v1/search?q=" + URLEncoder.encode(songName, "utf-8") + "&type=track" + "&limit=5"))
+                .header("Authorization", "Bearer " + token)
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        int statusCode = response.statusCode();
+
+        if (statusCode == 200) {
+            String responseBody = response.body();
+            JSONObject jsonObject = new JSONObject(responseBody);
+            System.out.println(responseBody);
+            return responseBody;
+        }
+        return null;
+    }
+
+    public String getArtistFromID(String token, String artistID) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.spotify.com/v1/artists/" + artistID))
+                .header("Authorization", "Bearer " + token)
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        int statusCode = response.statusCode();
+        System.out.println(statusCode);
+
+        if (statusCode == 200) {
+            String responseBody = response.body();
+            JSONObject jsonObject = new JSONObject(responseBody);
+            System.out.println(responseBody);
+            return responseBody;
+        }
+        return null;
+    }
+
+    public String getTrackFromID(String token, String trackID) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.spotify.com/v1/tracks/" + trackID))
+                .header("Authorization", "Bearer " + token)
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        int statusCode = response.statusCode();
+        System.out.println(statusCode);
+
+        if (statusCode == 200) {
+            String responseBody = response.body();
+            JSONObject jsonObject = new JSONObject(responseBody);
+            System.out.println(responseBody);
             return responseBody;
         }
         return null;
--- src/main/java/edu/bsu/cs/Main.java
+++ src/main/java/edu/bsu/cs/Main.java
@@ -5,9 +5,11 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         Controller controller =  new Controller();
         //controller.spotifyInput();
+
+        //tests for sam
         API_Requests APIRequests = new API_Requests();
         Access access = new Access();
-        System.out.println(APIRequests.searchForArtist(Access.getAccessToken(), "Juice WLRD"));
+        System.out.println(APIRequests.getTrackFromID(Access.getAccessToken(), "3cWmqvMwVQKDigWLSZ3w9h"));
 
     }
 
