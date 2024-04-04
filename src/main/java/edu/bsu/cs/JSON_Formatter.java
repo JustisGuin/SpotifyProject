5d1736fe8bd6591e2cf259273f6adb7cad300bcf
joemitchell <joellehctim@gmail.com>
2024-04-03 22:12:30 -0400

    Fixes JSON_Formatter so that it will return data and be printed through a View class. It also configures these classes with the Controller class.

--- src/main/java/edu/bsu/cs/JSON_Formatter.java
+++ src/main/java/edu/bsu/cs/JSON_Formatter.java
@@ -1,142 +1,97 @@
 package edu.bsu.cs;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class JSON_Formatter {
-    static final Access access = new Access();
 
-    //FORMAT ARTISTS
     public static String formatArtist(String responseBody) {
         JSONObject jsonObject = new JSONObject(responseBody);
         JSONObject artistsObject = jsonObject.getJSONObject("artists");
-        System.out.println();
-        System.out.println("Showing the top 3 responses of your query:");
-        System.out.println();
+        StringBuilder formattedOutput = new StringBuilder();
+        formattedOutput.append("\nShowing the top 3 responses of your query:\n\n");
         if (artistsObject.has("items")) {
-            org.json.JSONArray itemsArray = artistsObject.getJSONArray("items");
-            for (int i = 0; i < itemsArray.length(); i++) {
+            JSONArray itemsArray = artistsObject.getJSONArray("items");
+            for (int i = 0; i < Math.min(3, itemsArray.length()); i++) {
                 JSONObject artistObject = itemsArray.getJSONObject(i);
-                formatName(artistObject);
-                formatID(artistObject);
-                formatGenres(artistObject);
-                //formatPopularity(artistObject);
-                System.out.println();
+                formattedOutput.append(formatName(artistObject)).append("\n");
+                formattedOutput.append(formatID(artistObject)).append("\n");
+                formattedOutput.append(formatGenres(artistObject)).append("\n");
+                formattedOutput.append(formatPopularity(artistObject)).append("\n\n");
             }
-            if (itemsArray.isEmpty()){
-                System.out.println("No results found!");
+            if (itemsArray.isEmpty()) {
+                formattedOutput.append("No results found!\n");
             }
         }
-        return null;
+        return formattedOutput.toString();
     }
 
-    public static String formatName(JSONObject artistObject){
-        String artistName = artistObject.getString("name");
-        System.out.println("Artist Name: " + artistName);
-        return artistName;
+    public static String formatName(JSONObject artistObject) {
+        return "Artist Name: " + artistObject.getString("name");
     }
 
     public static String formatID(JSONObject artistObject) {
-        String artistId = artistObject.getString("id");
-        System.out.println("Artist ID: " + artistId);
-        return artistId;
+        return "Artist ID: " + artistObject.getString("id");
     }
 
     public static String formatGenres(JSONObject artistObject) {
-        org.json.JSONArray genresArray = artistObject.getJSONArray("genres");
-        System.out.println("Artist genres:");
+        StringBuilder genresOutput = new StringBuilder("Artist genres:\n");
+        JSONArray genresArray = artistObject.getJSONArray("genres");
         for (int j = 0; j < genresArray.length(); j++) {
-            String genre = genresArray.getString(j);
-            System.out.println("- " + genre);
+            genresOutput.append("- ").append(genresArray.getString(j)).append("\n");
         }
-        return String.valueOf(genresArray);
-
+        return genresOutput.toString();
     }
 
-
-
-    //FORMAT TRACKS
-
+    public static String formatPopularity(JSONObject artistObject) {
+        return "Artist popularity: " + artistObject.getInt("popularity");
+    }
 
     public static List<String> formatTrack(String responseBody) {
-        List<String> formattedSearch = new ArrayList<>();
-        List<String> trackNames = new ArrayList<>();
-        List<String> trackArtists = new ArrayList<>();
+        List<String> formattedTracks = new ArrayList<>();
         JSONObject jsonObject = new JSONObject(responseBody);
         JSONObject tracksObject = jsonObject.getJSONObject("tracks");
         if (tracksObject.has("items")) {
-            org.json.JSONArray itemsArray = tracksObject.getJSONArray("items");
-            for (int i = 0; i < itemsArray.length(); i++) {
+            JSONArray itemsArray = tracksObject.getJSONArray("items");
+            for (int i = 0; i < Math.min(5, itemsArray.length()); i++) {
                 JSONObject trackObject = itemsArray.getJSONObject(i);
-                trackNames.add(formatTrackName(trackObject));
-                trackArtists.add(formatTrackArtists(trackObject));
-
+                formattedTracks.add(formatTrackInfo(trackObject, i + 1));
             }
-            for(int i = 0; i< trackNames.size(); i++) {
-                String combiner = (trackNames.get(i)+"\n"+trackArtists.get(i));
-                formattedSearch.add(combiner);
-
+            if (itemsArray.isEmpty()) {
+                formattedTracks.add("No results found!");
             }
-            if (itemsArray.isEmpty()){
-                System.out.println("No results found!");
-            }
-        }return formattedSearch;
+        }
+        return formattedTracks;
     }
 
-    public static String formatTrackName(JSONObject trackObject) {
+    public static String formatTrackInfo(JSONObject trackObject, int index) {
         String trackName = trackObject.getString("name");
-        String label =("Track name: " + trackName);
-        return label;
-
+        JSONArray artistsArray = trackObject.getJSONArray("artists");
+        String artistName = artistsArray.getJSONObject(0).getString("name");
+        return index + ":\nTrack name: " + trackName + "\nArtist Name: " + artistName;
     }
 
-    public static String formatTrackAlbum(JSONObject trackObject) {
-        String trackAlbum = trackObject.getString("album");
-        String label =("Track album: " + trackAlbum);
-        System.out.print(label);
-        return label;
-    }
-
-    public static String formatTrackArtists(JSONObject trackObject) {
-        org.json.JSONArray trackArtistsArray = trackObject.getJSONArray("artists");
-        JSONObject trackArtist = trackArtistsArray.getJSONObject(0);
-        String artistName = trackArtist.getString("name");
-        return ("Artist Name: "+artistName);
-    }
-
-    //FORMAT ALBUMS
     public static String formatAlbum(String responseBody) {
+        StringBuilder formattedOutput = new StringBuilder();
         JSONObject jsonObject = new JSONObject(responseBody);
         JSONObject albumsObject = jsonObject.getJSONObject("albums");
         if (albumsObject.has("items")) {
-            org.json.JSONArray itemsArray = albumsObject.getJSONArray("items");
+            JSONArray itemsArray = albumsObject.getJSONArray("items");
             for (int i = 0; i < itemsArray.length(); i++) {
                 JSONObject albumObject = itemsArray.getJSONObject(i);
-                //formatAlbumName(albumObject);
-
-
+                formattedOutput.append(formatAlbumID(albumObject)).append("\n");
             }
-            if (itemsArray.isEmpty()){
-                System.out.println("No results found!");
+            if (itemsArray.isEmpty()) {
+                formattedOutput.append("No results found!\n");
             }
         }
-        return responseBody;
+        return formattedOutput.toString();
     }
 
-    //public static String formatAlbumName(JSONObject albumObject){
-        //String albumName = albumObject.getString("name");
-        //return ("Album Name:"+albumName);
-    //}
-
-    public static String formatAlbumID(JSONObject albumObject){
-        String albumID = albumObject.getString("id");
-        System.out.println("album id: " + albumID);
-        return albumID;
+    public static String formatAlbumID(JSONObject albumObject) {
+        return "Album ID: " + albumObject.getString("id");
     }
-
-
-    }
-
-
-
+}
